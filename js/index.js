47d49583f8b64a421d7df258a46028891d4dc305
volumnet <info@volumnet.ru>
2013-09-22 21:59:03 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,21 @@
-var counter = 0;
-
-var onSuccess = function(acceleration) {
-    $('#inputAccelX').val(Math.round(acceleration.x * 10) / 10);
-    $('#inputAccelY').val(Math.round(acceleration.y * 10) / 10);
-    $('#inputAccelZ').val(Math.round(acceleration.z * 10) / 10);
-    $('#status').text('OK').css(color: 'green');
-    $('#counter').text(++counter);
-};
-
-var onError = function() {
-    $('#status').text('ERROR').css(color: 'red');
-};
-
-var accelTest = function() {
-    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-}
-setInterval(accelTest, 2000)
+jQuery(function($) {
+    alert('aaa');
+    var counter = 0;
+    
+    var onSuccess = function(acceleration) {
+        $('#inputAccelX').val(Math.round(acceleration.x * 10) / 10);
+        $('#inputAccelY').val(Math.round(acceleration.y * 10) / 10);
+        $('#inputAccelZ').val(Math.round(acceleration.z * 10) / 10);
+        $('#status').text('OK').css(color: 'green');
+        $('#counter').text(++counter);
+    };
+    
+    var onError = function() {
+        $('#status').text('ERROR').css(color: 'red');
+    };
+    
+    var accelTest = function() {
+        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    }
+    setInterval(accelTest, 2000)
+});
