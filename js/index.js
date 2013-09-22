8c46763c1662eb7f31a61ca33ff5c3fe01d9a313
Alex V. Surnin <info@volumnet.ru>
2013-09-22 19:58:56 +0600

    .

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var onSuccess = function(acceleration) {
-    document.getElementById('inputAccelX').value = Math.round(acceleration.x * 1000) / 1000;
-    document.getElementById('inputAccelY').value = Math.round(acceleration.y * 1000) / 1000;
-    document.getElementById('inputAccelZ').value = Math.round(acceleration.z * 1000) / 1000;
+    document.getElementById('inputAccelX').value = Math.round(acceleration.x * 1000);
+    document.getElementById('inputAccelY').value = Math.round(acceleration.y * 1000);
+    document.getElementById('inputAccelZ').value = Math.round(acceleration.z * 1000);
 };
 
 var onError = function() {
