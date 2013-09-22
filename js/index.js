a6eb8a4b9dbb45437e59bbda90d148fb08e098df
Alex V. Surnin <info@volumnet.ru>
2013-09-22 19:53:23 +0600

    .

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
-var app = function() {
-    var onSuccess = function(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-    };
-
-    var onError = function() {
-        alert('onError!');
-    };
-
-    var accelTest = function() {
-        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-    }
-
+var onSuccess = function(acceleration) {
+    document.getElementById('inputAccel').value = acceleration.x + ' x ' + acceleration.y + ' x ' + acceleration.z + ' x '      + acceleration.timestamp;
 };
+
+var onError = function() {
+    alert('onError!');
+};
+
+var accelTest = function() {
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
+document.getElementById('aAccel').onclick = accelTest;
