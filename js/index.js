0f9e30d73685dedf6785819295c4d605a65ed9f4
volumnet <info@volumnet.ru>
2013-09-22 23:11:26 +0500

    .

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 jQuery(function($) {
-    alert('aaa');
     var counter = 0;
     
     var onSuccess = function(acceleration) {
         $('#inputAccelX').val(Math.round(acceleration.x * 10) / 10);
         $('#inputAccelY').val(Math.round(acceleration.y * 10) / 10);
         $('#inputAccelZ').val(Math.round(acceleration.z * 10) / 10);
-        $('#status').text('OK').css(color: 'green');
+        $('#status').text('OK').css({color: 'green'});
         $('#counter').text(++counter);
     };
     
     var onError = function() {
-        $('#status').text('ERROR').css(color: 'red');
+        $('#status').text('ERROR').css({color: 'red'});
     };
     
     var accelTest = function() {
